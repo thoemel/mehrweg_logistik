2591d12d27d51d26d68f4a6e30902d0bc2dd0dca
thoemel <thoemel@thoemel.ch>
2014-06-09 22:55:18 +0200

    SQL-Script zum Tabellen truncaten vor dem Start des Projekts.

---
+++ gruenetatze/update/init.sql
@@ -0,0 +1,14 @@
+-- GRUENETATZE
+-- Datenbank bereit machen zum Start
+-- Dieses Script leert alle Tabellen, die im Betrieb gefüllt werden.
+-- "firma", "rolle", "users" und "ware" bleiben unangetastet.
+
+TRUNCATE TABLE `abrechnung`;
+
+TRUNCATE TABLE `bestand`;
+
+TRUNCATE TABLE `modifikation`;
+
+TRUNCATE TABLE `statistik`;
+
+
